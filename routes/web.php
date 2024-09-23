331b165d8e2a0894a7fdb5b6360f4a574d6a6da7
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-09-23 09:30:45 +0200

    Create Route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\JiriController::class, 'index'])->name('jiri.home');
+Route::get('/jiris', [App\Http\Controllers\JiriController::class, 'index'])->name('jiri.index');
+Route::get('/jiris/{jiri}', [App\Http\Controllers\JiriController::class, 'show'])->name('jiri.show');
+Route::get('/jiri/create', [App\Http\Controllers\JiriController::class, 'create'])->name('jiri.create');
+Route::post('/jiris', [App\Http\Controllers\JiriController::class, 'store'])->name('jiri.store');
+
+Route::get('/contacts', [App\Http\Controllers\ContactController::class, 'index'])->name('contacts.index');
+Route::get('/contacts/{id}', [App\Http\Controllers\ContactController::class, 'show'])->name('contact.show');
+
+Route::get('/projects', [App\Http\Controllers\ProjectController::class, 'index'])->name('projects.index');
+Route::get('/projects/{id}', [App\Http\Controllers\ProjectController::class, 'show'])->name('project.show');

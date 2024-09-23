e7c597d40e584d90f5da6f1454272fa7acbfab8f
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-09-23 09:30:32 +0200

    Create Factories

---
+++ database/seeders/JiriSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Jiri;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JiriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Jiri::factory(10)->create();
+    }
+}
